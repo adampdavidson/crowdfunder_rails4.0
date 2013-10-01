93a4f8186ff873cca854b91a90b662cd1c9bec51
adampdavidson <adam.davidson94@gmail.com>
2013-10-01 08:19:14 -0400

    Writing tests for browsing projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Crowdfunder::Application.routes.draw do
+
+  resources :users, :except => [:index]
+  resources :projects
+
+  root :to => "welcome#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
